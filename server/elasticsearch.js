4796f35b533006824267fd9edb347cb29a8fea5c
Amelia Brown <amelia.brown389@gmail.com>
2016-10-30 17:11:01 -0700

    (fix): fix syntax error

--- server/elasticsearch.js
+++ server/elasticsearch.js
@@ -1,5 +1,5 @@
 const elasticsearch = require('elasticsearch');
-const host = process.env.ELASTICSEARCH : 'localhost';
+const host = process.env.ELASTICSEARCH || 'localhost';
 const port = '9200'
 
 const client = new elasticsearch.Client({
